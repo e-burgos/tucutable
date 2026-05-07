de5a0e58459fc3a59e4f1435116c97300650bbfe
Esteban Burgos <estebanburgos85@gmail.com>
2026-05-07 15:46:58 -0300

    feat(demo): set standalone architecture with horizontal layout in ThemeProvider

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ThemeProvider } from '@e-burgos/tucu-ui';
+import { LAYOUT_OPTIONS, ThemeProvider } from '@e-burgos/tucu-ui';
 import { useMenuItems } from './router/menuItems';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -12,6 +12,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
+        architecturalPatterns="standalone"
+        layout={LAYOUT_OPTIONS.HORIZONTAL}
         showSettings={false}
         rightButton={<RightButton menuItems={menuItems} />}
         logo={{
